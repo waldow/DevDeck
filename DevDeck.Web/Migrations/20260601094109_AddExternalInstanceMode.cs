865f1b1845de13c64fbade3275694235c2caf3f2
Waldo <waldow@gmail.com>
2026-06-01 19:21:09 +0200

    Migrations

---
+++ DevDeck.Web/Migrations/20260601094109_AddExternalInstanceMode.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevDeck.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExternalInstanceMode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ExternalPort",
+                table: "DevServices",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "UseExternalInstance",
+                table: "DevServices",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExternalPort",
+                table: "DevServices");
+
+            migrationBuilder.DropColumn(
+                name: "UseExternalInstance",
+                table: "DevServices");
+        }
+    }
+}
